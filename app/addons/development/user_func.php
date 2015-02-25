d567aefa05df770fc6f01fb1e8f6faf6f5d76216
Paul Dreada <pauldreada@gmail.com>
2015-02-25 22:17:09 +0300

    Watermark. shoes

--- app/addons/development/user_func.php
+++ app/addons/development/user_func.php
@@ -39,11 +39,11 @@ function fn_update_product_exception($product_id, $product_options, $new_amount)
 
 function fn_update_product_exceptions($product_id, $combinations)
 {
-    db_query("DELETE FROM ?:product_options_exceptions WHERE product_id = ?i", $product_id);
     if (!empty($combinations)) {
         $combination_options = db_get_hash_single_array("SELECT combination, combination_hash FROM ?:product_options_inventory WHERE combination_hash IN (?n)", array('combination_hash', 'combination'), array_keys($combinations));
         if (!empty($combination_options)) {
             foreach ($combination_options as $hash => $combination) {
+                db_query("DELETE FROM ?:product_options_exceptions WHERE product_id = ?i AND combination = ?s", $product_id, serialize(fn_get_product_options_by_combination($combination)));
                 if (!empty($combinations[$hash]) && $combinations[$hash]['amount'] < 1) {
                     $_data = array(
                         'product_id' => $product_id,
--- design/themes/responsive/templates/addons/development/blocks/static_templates/ssl_seal.tpl
+++ design/themes/responsive/templates/addons/development/blocks/static_templates/ssl_seal.tpl
@@ -1,4 +1,5 @@
 {if $show_tooltip}<span class="ty-tooltip-block"><a class="cm-tooltip" data-ceTooltipPosition="ssl" title="{__('ssl_seal_text')}">{/if}<div class="ty-ssl-cert" id="ssl-cert"></div>{if $show_tooltip}</a></span>{/if}
+{if $smarty.session.display_ssl_tooltip == 'Y'}
 <script type="text/javascript">
     Tygh.$(document).ready(function() {$ldelim}
         setTimeout(function() {$ldelim}
@@ -9,3 +10,4 @@
         {$rdelim}, 5000);
     {$rdelim});
 </script>
+{/if}
