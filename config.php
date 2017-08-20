6802c3b17f46237f34d62a1367bc657d34bd53e7
Paul Dreada <pauldreada@gmail.com>
2017-08-20 17:16:15 +0300

    routine

--- config.php
+++ config.php
@@ -152,7 +152,7 @@ define('SESS_VALIDATE_UA', true); // link session ID with user-agent
 
 define('BILLING_ADDRESS_PREFIX', 'b');
 define('SHIPPING_ADDRESS_PREFIX', 's');
-define('DEBUGGING_IP', '95.24.238.226');
+define('DEBUGGING_IP', '95.28.52.26');
 
 /*
  * Dynamic options
