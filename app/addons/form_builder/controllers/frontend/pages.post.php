8e8e714674704add1b997d4fdfe247c5b34316da
Paul Dreada <pauldreada@gmail.com>
2019-06-29 21:47:42 +0300

    fix https

--- app/addons/form_builder/controllers/frontend/pages.post.php
+++ app/addons/form_builder/controllers/frontend/pages.post.php
@@ -37,8 +37,8 @@ if ($mode == 'view' && !empty($_REQUEST['page_id'])) {
     if (!defined('HTTPS') && $page_is_https == 'Y') {
         return array(CONTROLLER_STATUS_REDIRECT, Registry::get('config.https_location') . '/' . Registry::get('config.current_url'));
 
-    } elseif (defined('HTTPS') && Registry::get('settings.Security.keep_https') != 'Y' && $page_is_https != 'Y') {
-        return array(CONTROLLER_STATUS_REDIRECT, Registry::get('config.http_location') . '/' . Registry::get('config.current_url'));
+//     } elseif (defined('HTTPS') && Registry::get('settings.Security.keep_https') != 'Y' && $page_is_https != 'Y') {
+//         return array(CONTROLLER_STATUS_REDIRECT, Registry::get('config.http_location') . '/' . Registry::get('config.current_url'));
     }
 
     $restored_form_values = fn_restore_post_data('form_values');
